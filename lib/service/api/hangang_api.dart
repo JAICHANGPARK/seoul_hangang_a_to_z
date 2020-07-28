36edf26ee57b178fbfe6723c01f26c1b1e754811
Dreamwalker <aristojeff@gmail.com>
2020-07-28 14:35:14 +0900

    Update hangang_api.dart

--- lib/service/api/hangang_api.dart
+++ lib/service/api/hangang_api.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
 import 'package:flutter_seoul_hangan_a_to_z/model/hangang.dart';
+import 'package:flutter_seoul_hangan_a_to_z/model/hangang_level.dart';
+import 'package:flutter_seoul_hangan_a_to_z/model/simple_hangang.dart';
 import 'package:http/http.dart' as http;
 
 Future<Hangang> fetchSeoulHangangData() async{
   var response = await http.get("http://openapi.seoul.go.kr:8088/4d714f6e477061723533694d6b6f45/json/WPOSInformationTime/0/5/20200728");
   if(response.statusCode == 200){
-    print(response.toString());
     Hangang hangang = Hangang.fromJson(jsonDecode(response.body));
     return hangang;
   }else{
@@ -14,3 +15,23 @@ Future<Hangang> fetchSeoulHangangData() async{
   }
 }
 
+
+Future<SimpleHangang> fetchSimpleHangangData() async{
+  var response = await http.get("http://hangang.dkserver.wo.tc/");
+  if(response.statusCode == 200){
+    SimpleHangang hangang = SimpleHangang.fromJson(jsonDecode(response.body));
+    return hangang;
+  }else{
+    return null;
+  }
+}
+
+Future<HangangLevel> fetchHangangLevelData() async{
+  var response = await http.get("http://openapi.seoul.go.kr:8088/4d714f6e477061723533694d6b6f45/json/ListRiverStageService/1/20/");
+  if(response.statusCode == 200){
+    HangangLevel hangang = HangangLevel.fromJson(jsonDecode(response.body));
+    return hangang;
+  }else{
+    return null;
+  }
+}
